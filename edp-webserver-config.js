097cae4615c78377371d0466a16c7fe17b74c1ee
otakustay <otakustay@gmail.com>
2016-06-19 19:44:18 +0800

    初始化项目

---
+++ edp-webserver-config.js
@@ -0,0 +1,50 @@
+/**
+ * @file config edp-webserver
+ * @author EFE
+ */
+
+/* globals home, redirect, content, empty, autocss, file, less, stylus, proxyNoneExists */
+
+exports.port = 8848;
+exports.directoryIndexes = true;
+exports.documentRoot = __dirname;
+
+var babelCore = require('babel-core');
+
+exports.getLocations = function () {
+    return [
+        {
+            location: /\.css($|\?)/,
+            handler: [
+                autocss()
+            ]
+        },
+        {
+            location: /\.less($|\?)/,
+            handler: [
+                file(),
+                less()
+            ]
+        },
+        {
+            location: /^\/(src|dep)\/.*(\.js|\.js\.map)($|\?)/,
+            handler: [
+                babel({sourceMaps: 'inline'}, {babel: babelCore})
+            ]
+        },
+        {
+            location: /^.*$/,
+            handler: [
+                file(),
+                proxyNoneExists()
+            ]
+        }
+    ];
+};
+
+/* eslint-disable guard-for-in */
+exports.injectResource = function (res) {
+    for (var key in res) {
+        global[key] = res[key];
+    }
+};
